ac94cc6d5d70799da39ef0acc7714b501838f327
pmartynov <paul.martynov@gmail.com>
2013-09-29 16:42:45 +0300

    test commit

--- converter_app/cache_wrappers.py
+++ converter_app/cache_wrappers.py
@@ -18,8 +18,8 @@ def get_currencies():
 def get_exchange_rates():
     exchange_rates = cache.get(EXCHANGE_RATES)
     if not exchange_rates:
-        exchange_rates = ExchangeRate.objects.select_related("currency_from", "currency_to")\
-            .all().order_by("currency_from", "currency_to")
+        exchange_rates = ExchangeRate.objects.select_related("currency_from", "currency_to").all()\
+            .order_by("currency_from", "currency_to")
         cache.set(EXCHANGE_RATES, exchange_rates)
 
     return exchange_rates
