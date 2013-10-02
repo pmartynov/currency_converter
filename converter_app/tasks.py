b122f00db691f287f0358f37d5580a53c9836a6e
Gleb Pitsevich <pitsevich@gmail.com>
2013-10-02 14:44:42 +0300

    Keeping try-except as short as possible.

--- converter_app/tasks.py
+++ converter_app/tasks.py
@@ -12,15 +12,14 @@ import requests
 def update_currencies():
     try:
         currencies = requests.get(OER_CURRENCIES_URL).json()
-
-        for short, long in currencies.iteritems():
-            currency = Currency(short_name=short, long_name=long.encode('unicode_escape'))
-            currency.save()
-
-        transaction.commit()
-
     except requests.exceptions.ConnectionError:
-        pass
+        return
+
+    for short, long in currencies.iteritems():
+        currency = Currency(short_name=short, long_name=long.encode('unicode_escape'))
+        currency.save()
+
+    transaction.commit()
 
 
 @periodic_task(run_every=crontab(hour="0-23"))
@@ -28,16 +27,16 @@ def update_currencies():
 def update_rates():
     try:
         rates = requests.get(OER_LATEST_URL).json()["rates"]
-
-        for short, rate in rates.iteritems():
-            curr_from = Currency.objects.get(short_name="USD")
-            curr_to = Currency.objects.get(short_name=short)
-            ex_rate, created = ExchangeRate.objects.get_or_create(
-                currency_from=curr_from, currency_to=curr_to, defaults={'rate': 1.0})
-            ex_rate.rate = rate
-            ex_rate.save()
-
-        transaction.commit()
-
     except requests.exceptions.ConnectionError:
-        pass
+        return
+
+    for short, rate in rates.iteritems():
+        curr_from = Currency.objects.get(short_name="USD")
+        curr_to = Currency.objects.get(short_name=short)
+        ex_rate, created = ExchangeRate.objects.get_or_create(
+            currency_from=curr_from, currency_to=curr_to, defaults={'rate': 1.0})
+        ex_rate.rate = rate
+        ex_rate.save()
+
+    transaction.commit()
+
