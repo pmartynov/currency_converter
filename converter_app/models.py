14004975e57785b2fd9acf753ebe1c56917c8d80
Gleb Pitsevich <pitsevich@gmail.com>
2013-10-02 14:37:02 +0300

    Magic unique_together to reduce code lines.

--- converter_app/models.py
+++ converter_app/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 
 
 class Currency(models.Model):
@@ -24,13 +23,6 @@ class ExchangeRate(models.Model):
     currency_to = models.ForeignKey(Currency, related_name="+", unique=False)
     rate = models.DecimalField(max_digits=24, decimal_places=12, unique=False)
 
-    def validate_unique(self, *args, **kwargs):
-        super(ExchangeRate, self).validate_unique(*args, **kwargs)
-        ex_rate = self.__class__.objects.get(currency_from=self.currency_from, currency_to=self.currency_to)
-
-        if ex_rate.exists():
-            raise ValidationError('ExchangeRate row with these currencies already exists')
-
     def is_less_than(self, other):
         return self.currency_from.is_less_than(other.currency_from) or\
             self.currency_from == other.currency_from and self.currency_to.is_less_than(other.currency_to)
@@ -42,3 +34,6 @@ class ExchangeRate(models.Model):
     def __eq__(self, other):
         return isinstance(other, self.__class__) and self.currency_from == other.currency_from\
             and self.currency_to == other.currency_to
+
+    class Meta:
+        unique_together = ('currency_from', 'currency_to')
